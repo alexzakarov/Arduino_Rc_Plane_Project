f939a03526bc7c8e2ea1467dd6a05526776b4f29
MuhammedTalhaSemerci <35690789+MuhammedTalhaSemerci@users.noreply.github.com>
2020-09-21 01:37:05 +0300

    Add files via upload

---
+++ ucak_alici.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#include <nRF24L01.h> 
+#include <RF24.h>
+#include <Servo.h>
+
+#define CE_PIN   7
+#define CSN_PIN 8
+
+int x_hiz=0;
+int y_hiz=0;
+int z_hiz=0;
+
+Servo esc;
+Servo servo;
+Servo servo1;
+
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+RF24 radio(CE_PIN, CSN_PIN); 
+
+int data[3]; 
+
+void setup()   
+{
+esc.attach(5);
+servo.attach(2);
+servo1.attach(3);
+    
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();;
+}
+
+
+void loop() {
+  if ( radio.available() )
+  {
+    radio.read( data, sizeof(data) ); 
+    
+
+
+hareket(data[0],data[1],data[2]);
+
+  }
+}
+
+void hareket(int x_axis, int y_axis, int z_axis ) {
+ 
+ x_hiz= map(x_axis,0,1023,0,180);
+esc.write(x_hiz);
+ 
+ y_hiz= map(y_axis,0,1023,0,90);
+servo.write(y_hiz);
+
+z_hiz= map(z_axis,0,1023,0,75);
+servo1.write(z_hiz);
+
+
+  }
+
---
+++ ucak_verici.ino
@@ -0,0 +1,37 @@
+#include <SPI.h> 
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+#define CE_PIN   7
+#define CSN_PIN 8
+
+#define x_axis A0 // x axis
+#define y_axis A1 //y axis
+#define z_axis A2
+
+
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+RF24 radio(CE_PIN, CSN_PIN); 
+
+int data[3];  
+
+void setup() 
+{
+
+  radio.begin();
+  radio.openWritingPipe(pipe);
+
+}
+
+void loop()   
+{
+
+  data[0] = analogRead(x_axis);
+  data[1] = analogRead(y_axis);
+  data[2] = analogRead(z_axis);
+  radio.write( data, sizeof(data) ); 
+
+  
+}
